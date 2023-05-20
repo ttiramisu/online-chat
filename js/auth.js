ac93a59230f96d07cf3c8b19f07c9ec73e8fec18
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-05-20 19:21:29 +0800

    update

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-    window.location.assign('chat.html')
+    
   } else {
     // User is not signed in, redirect to the login page
     window.location.replace('login.html');
