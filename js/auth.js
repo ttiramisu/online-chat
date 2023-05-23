284f3e2a7656bbfd9728ce6ec6d24af66b73fa29
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-05-23 22:23:35 +0800

    update

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+const firebaseInfo = {
   apiKey: "AIzaSyCc1RCwcQLgHCIbdAYQw-iyD9oRm0TfknM",
   authDomain: "chat-test-88207.firebaseapp.com",
   databaseURL: "https://chat-test-88207-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -8,7 +8,7 @@ const firebaseConfig = {
   appId: "1:475093710040:web:13a5dcf2b0b8aae81d53ad"
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseInfo);
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (!user) {
