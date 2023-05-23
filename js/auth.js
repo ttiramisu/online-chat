80ce64483b997df42da1d4ed6c5b8a6dad4eb683
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-05-23 22:22:26 +0800

    update

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,19 @@
-firebase.initializeApp(firebaseApp);
+const firebaseConfig = {
+  apiKey: "AIzaSyCc1RCwcQLgHCIbdAYQw-iyD9oRm0TfknM",
+  authDomain: "chat-test-88207.firebaseapp.com",
+  databaseURL: "https://chat-test-88207-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-test-88207",
+  storageBucket: "chat-test-88207.appspot.com",
+  messagingSenderId: "475093710040",
+  appId: "1:475093710040:web:13a5dcf2b0b8aae81d53ad"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (!user) {
-    window.location.replace('login.html');
+    window.location.replace('index.html');
+    localStorage.removeItem('userEmail');
   } else {
     localStorage.setItem('userEmail', user.email);
   }
