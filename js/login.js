06ed203eeee002a0b950443770d1e72a8aef3e61
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-11-12 23:08:35 +0800

    REMOVED API KEYS

--- js/login.js
+++ js/login.js
@@ -2,13 +2,13 @@ let showpwd;
 let ipAddress;
 
 const firebaseApp = {
-  apiKey: 'AIzaSyCc1RCwcQLgHCIbdAYQw-iyD9oRm0TfknM',
-  authDomain: "chat-test-88207.firebaseapp.com",
-  databaseURL: "https://chat-test-88207-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-test-88207",
-  storageBucket: "chat-test-88207.appspot.com",
-  messagingSenderId: "475093710040",
-  appId: "1:475093710040:web:13a5dcf2b0b8aae81d53ad"
+  apiKey: 'API KEY',
+  authDomain: "AUTHDOMAIN",
+  databaseURL: "DB URL",
+  projectId: "PROJECT ID",
+  storageBucket: "STORAGE BUCKET",
+  messagingSenderId: "MSG SENDER ID",
+  appId: "APP ID"
 };
 
 firebase.initializeApp(firebaseApp);
--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Retrieve user email from local storage
 const userEmail = localStorage.getItem('userEmail');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCc1RCwcQLgHCIbdAYQw-iyD9oRm0TfknM',
-  authDomain: "chat-test-88207.firebaseapp.com",
-  databaseURL: "https://chat-test-88207-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-test-88207",
-  storageBucket: "chat-test-88207.appspot.com",
-  messagingSenderId: "475093710040",
-  appId: "1:475093710040:web:13a5dcf2b0b8aae81d53ad"
+const firebaseApp = {
+  apiKey: 'API KEY',
+  authDomain: "AUTHDOMAIN",
+  databaseURL: "DB URL",
+  projectId: "PROJECT ID",
+  storageBucket: "STORAGE BUCKET",
+  messagingSenderId: "MSG SENDER ID",
+  appId: "APP ID"
 };
 
 // Initialize Firebase
